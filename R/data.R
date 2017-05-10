92826fde7a75377fadf434eaad5680e42d64ff26
Bradley Boehmke <bradleyboehmke@gmail.com>
2017-05-10 13:40:40 -0400

    added advertising data

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Advertising budgets and newspaper sales for 200 markets.
+#'
+#' A dataset containing the advertising budgets and newspaper sales for 200 markets.
+#'
+#' @format A data frame with 200 rows and 4 variables:
+#' \describe{
+#'   \item{TV}{TV media budget, in thousands of US dollars}
+#'   \item{Radio}{Radio media budget, in thousands of US dollars}
+#'   \item{Newspaper}{Newspaper media budget, in thousands of US dollars}
+#'   \item{Sales}{unit sales, in thousands}
+#' }
+#' @source \url{http://www-bcf.usc.edu/~gareth/ISL}
+"advertising"
