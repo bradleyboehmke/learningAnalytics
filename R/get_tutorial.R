221c7d28c8642161730db01c443989146575148a
Brad Boehmke <bradleyboehmke@gmail.com>
2017-05-10 22:39:13 -0400

    added resampling & model selection tutorials

--- R/get_tutorial.R
+++ R/get_tutorial.R
@@ -10,6 +10,8 @@
 #'       \item \code{"Unsupervised"}: Principal Components Analysis & Cluster Analysis
 #'       \item \code{"Linear Regression"}: Linear Regression
 #'       \item \code{"Supervised Classification"}: Logistic Regression & Discriminant Analysis
+#'       \item \code{"Resampling"}: Leave-One-Out Cross-Validation, *k*-Fold Cross Validation, & Bootstrapping
+#'       \item \code{"Model Selection"}: Best Subset & Stepwise Selection for Linear Models
 #'       }
 #'
 #'
@@ -28,6 +30,7 @@ get_tutorial <- function(tutorial) {
 
   if(tutorial == "Linear Regression") tutorial <- "LinearRegression"
   if(tutorial == "Supervised Classification") tutorial <- "SupervisedClassification"
+  if(tutorial == "Model Selection") tutorial <- "ModelSelection"
 
   learnr::run_tutorial(tutorial, package = "learningAnalytics")
 }
